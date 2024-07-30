b25aa1301c4483ebc5f7e7d69df4d58864c0801b
jitesh yadav <jiteshkumaryadav99@gmail.com>
2024-07-30 17:29:46 +0530

    add url

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
     cors({
-        origin: ["https://dashboard-two-drab.vercel.app/","https://zerodhclone.vercel.app/"],
+        origin: ["https://dashboard-two-drab.vercel.app","https://zerodhclone.vercel.app"],
         methods: ["GET", "POST", "UPDATE", "DELETE"],
         credentials: true
     })
